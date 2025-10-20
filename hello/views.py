36180e1ab14d18cf0be6e65592e1e9eb585e4727
wangdi0214 <3264203557@qq.com>
2025-10-20 20:21:04 +0800

    修改Hello World视图显示内容为20231201034+王迪
    - 更新所有视图函数显示内容
    - 首页显示20231201034+王迪
    - 个性化页面显示20231201034+王迪 - 欢迎 {name}!
    - 模板页面标题和内容更新为20231201034+王迪

--- hello/views.py
+++ hello/views.py
@@ -3,17 +3,17 @@ from django.shortcuts import render
 
 def hello_world(request):
     """简单的Hello World视图"""
-    return HttpResponse("<h1>Hello World!</h1><p>欢迎来到Django Hello World应用！</p>")
+    return HttpResponse("<h1>20231201034+王迪</h1><p>这是我的Django Hello World应用！</p>")
 
 def hello_name(request, name):
     """带参数的Hello视图"""
-    return HttpResponse(f"<h1>Hello {name}!</h1><p>很高兴见到你！</p>")
+    return HttpResponse(f"<h1>20231201034+王迪 - 欢迎 {name}!</h1><p>很高兴见到你！</p>")
 
 def hello_template(request):
     """使用模板的Hello视图"""
     context = {
-        'title': 'Hello World Template',
-        'message': '这是一个使用模板的Hello World页面！',
-        'features': ['Django框架', 'Python语言', 'HTML模板']
+        'title': '20231201034+王迪',
+        'message': '这是我的Django Hello World应用！',
+        'features': ['Django框架', 'Python语言', 'HTML模板', '20231201034+王迪']
     }
     return render(request, 'hello/hello.html', context)
